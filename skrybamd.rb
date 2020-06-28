568dbc4744d6aa3ff77ae20ac760b13fd164c2cf
Robert Herdzik <robert.herdzik@wallapop.com>
2020-06-28 16:53:00 +0200

    Skryba md configuration

---
+++ skrybamd.rb
@@ -0,0 +1,11 @@
+class SkrybaMD < Formula
+  desc "Swifty powered 🔌Markdown Documentation generator. If your team need easy way to maintain and create documentation, this generator is for you."
+  homepage "https://github.com/robertherdzik/SkrybaMD"
+  url "https://github.com/robertherdzik/SkrybaMD.git",
+      :tag => "0.0.4"
+  head "https://github.com/bitomule/SwiftyPods.git"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+end

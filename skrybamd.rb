b2ad93e80af0276557cf081db9172f88897b8a55
Robert Herdzik <robert.herdzik@yahoo.com>
2020-06-29 09:02:57 +0200

    Update version

--- skrybamd.rb
+++ skrybamd.rb
@@ -2,7 +2,7 @@ class Skrybamd < Formula
   desc "Swifty powered 🔌Markdown Documentation generator. If your team need easy way to maintain and create documentation, this generator is for you."
   homepage "https://github.com/robertherdzik/SkrybaMD"
   url "https://github.com/robertherdzik/SkrybaMD.git",
-      :tag => "0.0.5"
+      :tag => "0.0.6"
   head "https://github.com/bitomule/SwiftyPods.git"
 
   def install
